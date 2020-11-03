217e59f28d33deeb1ac3ca08886cd9a9adae8e43
litt <litt@ru-litt.ru>
2020-11-03 14:43:41 +0300

    Installer script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+dir=backup
+i=0
+while [ -d $dir$i ]; do
+  i=$((i+1))
+done
+mkdir $dir$i
+mkdir $dir$i/.config
+mkdir -p $dir$i/.local/share/applications
+mkdir $dir$i/.local/bin
+
+for f in $(find .* \( -prune ! -name ".local" ! -name ".config" ! -name ".git*" ! -name "." ! -name ".." \) && find .config/* -prune && find .local -type f); do
+  cp -a ~/$f $dir$i/$f
+  rm -rf ~/$f
+  ln -s $(dirname $(realpath $0))/$f ~/$f
+done
+exit 0
